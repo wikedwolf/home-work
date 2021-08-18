f39f9b1d272bdbd55804419efd13fb8f86eb481f
daria <dk.957@ya.ru>
2021-08-18 23:40:34 +0300

    Добавлен тест для урока 4 на проверку корректного заполнения коллекции.

--- src/test/java/com/sbrf/reboot/repository/AccountRepositoryImplTest.java
+++ src/test/java/com/sbrf/reboot/repository/AccountRepositoryImplTest.java
@@ -5,13 +5,29 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AccountRepositoryImplTest {
 
     AccountRepository accountRepository;
+
+
+    @Test
+    void onlyPersonalAccounts() throws IOException {
+        accountRepository = new AccountRepositoryImpl("src/main/resources/Accounts.txt");
+        Set<String> allAccountsByClientId = accountRepository.getAllAccountsByClientId(1);
+        ArrayList<String> strings = new ArrayList<String>() {{
+            add("2-ACCNUM");
+            add("1-ACCNUM");
+            add("4-ACC1NUM");
+        }};
+
+        assertTrue(strings.containsAll(allAccountsByClientId));
+    }
 
     @Test
     void successGetAllAccountsByClientId() throws IOException {
