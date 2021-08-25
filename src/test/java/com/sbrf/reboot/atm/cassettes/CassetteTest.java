63b1bfc9459efa218f95515a8353b1fc9c1e0fbb
daria <dk.957@ya.ru>
2021-08-25 15:54:09 +0300

    Добавлен тест для урока 5.

---
+++ src/test/java/com/sbrf/reboot/atm/cassettes/CassetteTest.java
@@ -0,0 +1,28 @@
+package com.sbrf.reboot.atm.cassettes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class CassetteTest {
+
+    class OneHundred extends Banknote {
+    }
+
+    class OneThousand extends Banknote {
+    }
+
+    @Test
+    void getCountBanknotes() {
+        OneHundred oneHundred = new OneHundred();
+
+        Cassette<OneHundred> cassette = new Cassette<>(new ArrayList<OneHundred>() {{
+            add(oneHundred);
+//            add(new OneThousand()); //it will not compile
+//            add(new Banknote()); //it will not compile
+        }});
+
+        Assertions.assertEquals(1, cassette.getCountBanknotes());
+    }
+}
