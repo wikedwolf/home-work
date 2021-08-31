3c00adb149ab4787af4d76afcaa939b6c12f894b
daria <dk.957@ya.ru>
2021-09-01 01:02:04 +0300

    Добавлен тест для урока 6-7.

---
+++ src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
@@ -0,0 +1,49 @@
+package com.sbrf.reboot.utils;
+
+import com.sbrf.reboot.dto.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+class AccountUtilsTest {
+
+    @Test
+    void sortedById() {
+        List<Account> accounts = new ArrayList<Account>() {{
+            add(Account.builder().id(3L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(1L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+        }};
+
+        AccountUtils.sortedById(accounts);
+
+        Assertions.assertEquals(1L, accounts.get(0).getId());
+        Assertions.assertEquals(2L, accounts.get(1).getId());
+        Assertions.assertEquals(3L, accounts.get(2).getId());
+        Assertions.assertEquals(3L, accounts.get(3).getId());
+
+    }
+
+    @Test
+    void sortedByIdDate() {
+        List<Account> accounts = new ArrayList<Account>() {{
+            add(Account.builder().id(1L).createDate(LocalDate.now().minusDays(4)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+        }};
+
+        AccountUtils.sortedByIdDate(accounts);
+
+        Assertions.assertEquals(1L, accounts.get(0).getId());
+        Assertions.assertEquals(2L, accounts.get(1).getId());
+        Assertions.assertEquals(LocalDate.now().minusDays(3), accounts.get(2).getCreateDate());
+        Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(3).getCreateDate());
+    }
+}
