513233818cdffc0f6a9356b5759c95f9a50ea08e
daria <dk.957@ya.ru>
2021-09-15 09:41:15 +0300

    Добавлен тест для урока 8-9.

--- src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
+++ src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
@@ -1,13 +1,19 @@
 package com.sbrf.reboot.service;
 
+import com.sbrf.reboot.dto.Account;
+import com.sbrf.reboot.repository.AccountRepository;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class AccountServiceTest {
@@ -24,6 +30,7 @@ class AccountServiceTest {
         accountService = new AccountService(accountRepository);
     }
 
+    @SneakyThrows
     @Test
     void bookExist() {
         Account account = new Account("ACC1234NUM");
@@ -35,6 +42,7 @@ class AccountServiceTest {
         assertTrue(accountService.isAccountExist(1L, account));
     }
 
+    @SneakyThrows
     @Test
     void bookNotExist() {
         Set<Account> accounts = new HashSet();
@@ -44,4 +52,53 @@ class AccountServiceTest {
 
         assertFalse(accountService.isAccountExist(1L, new Account("ACC456NUM")));
     }
+
+    @SneakyThrows
+    @Test
+    void getMaxAccountBalance() {
+        Account accountWithMaxBalance = Account.builder().clientId(1L).id(4L).balance(new BigDecimal(150000)).build();
+        Set<Account> accounts = new HashSet() {{
+            add(Account.builder().clientId(1L).id(1L).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(1L).id(2L).balance(new BigDecimal(200)).build());
+            add(Account.builder().clientId(1L).id(3L).balance(new BigDecimal("1.65")).build());
+            add(accountWithMaxBalance);
+        }};
+
+        when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
+
+        assertEquals(accountWithMaxBalance, accountService.getMaxAccountBalance(1L));
+    }
+
+
+    @SneakyThrows
+    @Test
+    void getAllAccountsByDateMoreThen() {
+        Account account1 = Account.builder().clientId(1L)
+                .createDate(LocalDate.now().minusDays(2))
+                .build();
+        Account account2 = Account.builder().clientId(1L)
+                .createDate(LocalDate.now().minusDays(3))
+                .build();
+        Account account3 = Account.builder().clientId(1L)
+                .createDate(LocalDate.now().minusDays(1))
+                .build();
+        Account account4 = Account.builder().clientId(1L)
+                .createDate(LocalDate.now().minusDays(7))
+                .build();
+
+        Set<Account> accounts = new HashSet() {{
+            add(account1);
+            add(account2);
+            add(account3);
+            add(account4);
+        }};
+
+        when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
+
+        Set allAccountsByDateMoreThen = accountService.getAllAccountsByDateMoreThen(1L, LocalDate.now().minusDays(2));
+
+        assertEquals(2, allAccountsByDateMoreThen.size());
+        assertTrue(allAccountsByDateMoreThen.contains(account3));
+    }
+
 }
