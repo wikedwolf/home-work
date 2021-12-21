e401181c5d12a0be8ba73b921d807faefd41b2b4
daria-pc <dk.957@ya.ru>
2021-12-21 12:11:24 +0300

    Переименован тесткласс для урока 4.

--- src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class AccountRepositoryImplTest {
+class FileAccountRepositoryTest {
 
     AccountRepository accountRepository;
 
