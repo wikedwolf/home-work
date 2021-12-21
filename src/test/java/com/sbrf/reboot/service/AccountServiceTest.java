0d2a16b63824a31a18a3c58e8d3c46222c5cd8c4
daria-pc <dk.957@ya.ru>
2021-12-21 11:16:42 +0300

    Исправлена директория для AccountRepository.

--- src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
+++ src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
@@ -1,5 +1,6 @@
 package com.sbrf.reboot.service;
 
+import com.sbrf.reboot.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
